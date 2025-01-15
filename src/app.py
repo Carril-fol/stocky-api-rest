8406589f476123342e013e1f7f9cdec8f6c7f749
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-15 19:58:06 -0300

    feat: Added controller from products

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from asgi import start_server
 from settings import settings_from_server, type_server
 from utils.extensions import jwt
+from controllers.product_controller import product_controller
 from controllers.category_controller import category_controller
 from controllers.supplier_controller import supplier_controller
 
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(settings_from_server[type_server])
 
 # Blueprints
+app.register_blueprint(product_controller)
 app.register_blueprint(category_controller)
 app.register_blueprint(supplier_controller)
 
