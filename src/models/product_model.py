d3376dd890f962b5b24e76b3abcc752a7d642e32
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-15 19:59:06 -0300

    feat: Update ProductModel to include description and date fields

--- src/models/product_model.py
+++ src/models/product_model.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 
 class ProductModel(BaseModel):
-    id: Optional[int] = Field(default=None, description='ID of the supplier')
-    name: Optional[str] = Field(default=None, min_length=3, max_length=50, description='Name of the supplier')
-    category_id: Optional[int] = Field(default=None, description='Id from the category')
+    id: Optional[int] = Field(default=None, description='ID of the product')
+    name: Optional[str] = Field(default=None, min_length=3, max_length=50, description='Name of the product')
+    description: Optional[str] = Field(default=None, min_length=3, max_length=300, description='Description of the product')
+    category_id: int = Field(default=None, description='Id from the category')
     status: Optional[str] = Field(default='active', description="Status of the product, defaults to 'active'")
+    date_creation: Optional[datetime] = Field(default_factory=datetime.now, description='Date of the creation product')
+    date_updated: Optional[datetime] = Field(default_factory=datetime.now, description='Date of the last update from the product')
 
     @field_validator('name')
     def name_must_contain_alphabet_characters(cls, value):
