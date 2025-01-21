2b1aaf2dc70f83efd06dfb2ecfcf022f1a843141
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-21 15:53:19 -0300

    refactor: Streamline product repository methods by leveraging base repository functions

--- src/repositories/product_repository.py
+++ src/repositories/product_repository.py
@@ -4,23 +4,16 @@ from entities.product_entity import ProductEntity
 class ProductRepository(Repository):
 
     def get_product_by_id(self, id: int):
-        with self.get_session() as session:
-            return session.query(ProductEntity).filter(ProductEntity.id == id).first() 
+        return self.get_register_entity(ProductEntity, id)
         
     def get_products(self):
-        with self.get_session() as session:
-            return session.query(ProductEntity).all()
+        return self.get_registers_entity(ProductEntity)
     
     def create_product(self, product: ProductEntity):
-        with self.get_session() as session:
-            session.add(product)
-            session.commit()
-            session.refresh(product)
-            return product
+        return self.create_register_entity(product)
     
     def update_product(self, product: ProductEntity):
-        with self.get_session() as session:
-            merged_product = session.merge(product)
-            session.commit()
-            session.refresh(merged_product)
-            return merged_product
+        return self.update_register_entity(product)
+    
+    def delete_logic_product(self, product: ProductEntity):
+        return self.delete_logic_register_entity(product)
