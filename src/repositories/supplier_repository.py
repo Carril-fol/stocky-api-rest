e3d9d9037b4a4d3abc6a7f4746c3b9d8c0f69153
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-21 15:55:09 -0300

    refactor: Simplify SupplierRepository methods by utilizing base repository functions

--- src/repositories/supplier_repository.py
+++ src/repositories/supplier_repository.py
@@ -4,34 +4,21 @@ from entities.supplier_entity import SupplierEntity
 class SupplierRepository(Repository):
 
     def get_suppliers_by_id(self, id: int):
-        with self.get_session() as session:
-            return session.query(SupplierEntity).filter(SupplierEntity.id == id).first()
+        return self.get_register_entity(SupplierEntity, id)
+    
+    def get_suppliers(self):
+        return self.get_registers_entity(SupplierEntity)
+
+    def create_supplier(self, supplier: SupplierEntity):
+        return self.create_register_entity(supplier)
+    
+    def update_supplier(self, supplier: SupplierEntity):
+        return self.update_register_entity(supplier)
+        
+    def delete_supplier(self, supplier: SupplierEntity):
+        return self.delete_logic_register_entity(supplier)
     
     def get_supplier_by_name(self, name: str):
         with self.get_session() as session:
             return session.query(SupplierEntity).filter(SupplierEntity.name == name).first()
         
-    def get_suppliers(self):
-        with self.get_session() as session:
-            return session.query(SupplierEntity).all()
-
-    def create_supplier(self, supplier: SupplierEntity):
-        with self.get_session() as session:
-            session.add(supplier)
-            session.commit()
-            session.refresh(supplier)
-            return supplier
-    
-    def update_supplier(self, supplier: SupplierEntity):
-        with self.get_session() as session:
-            merged_supplier = session.merge(supplier)
-            session.commit()
-            session.refresh(merged_supplier)
-            return merged_supplier
-        
-    def delete_supplier(self, supplier: SupplierEntity):
-        with self.get_session() as session:
-            merged_supplier = session.merge(supplier)
-            session.commit()
-            session.refresh(merged_supplier)
-            return merged_supplier
