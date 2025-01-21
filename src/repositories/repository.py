1b4bab9cb4d8ab1a1e3cce0494e43d11c0e00f96
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-21 15:53:27 -0300

    feat: Implement CRUD operations for register entities in the repository

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -10,3 +10,38 @@ class Repository:
             yield session
         finally:
             session.close()
+
+    def create_register_entity(self, entity):
+        with self.get_session() as session:
+            session.add(entity)
+            session.commit()
+            session.refresh(entity)
+            return entity
+    
+    def update_register_entity(self, entity):
+        with self.get_session() as session:
+            merged_entity = session.merge(entity)
+            session.commit()
+            session.refresh(merged_entity)
+            return merged_entity
+        
+    def get_register_entity(self, entity, id: int):
+        with self.get_session() as session:
+            return session.query(entity).filter(entity.id == id).first()
+        
+    def get_registers_entity(self, entity):
+        with self.get_session() as session:
+            return session.query(entity).all()
+        
+    def delete_register_entity(self, entity):
+        with self.get_session() as session:
+            session.delete(entity)
+            session.commit()
+            return True 
+    
+    def delete_logic_register_entity(self, entity):
+        with self.get_session() as session:
+            merged_entity = session.merge(entity)
+            session.commit()
+            session.refresh(merged_entity)
+            return merged_entity
