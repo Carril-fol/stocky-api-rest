23915b9116422ee3ddab9f943935b954490cbfaf
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-30 00:18:02 -0300

    refactor: Add supplier_id and relationship to SupplierEntity in ProductEntity for enhanced product management

--- src/entities/product_entity.py
+++ src/entities/product_entity.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship
 
 from database.db import Base
 from .category_entity import CategoryEntity
+from .supplier_entity import SupplierEntity
 
 class ProductEntity(Base):
     __tablename__ = 'products'
@@ -11,7 +12,9 @@ class ProductEntity(Base):
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'), nullable=False)
     status = Column(String, unique=False)
     date_creation = Column(DateTime, nullable=False, default=datetime.now())
     date_updated = Column(DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
     category = relationship(CategoryEntity)
+    supplier = relationship(SupplierEntity)
