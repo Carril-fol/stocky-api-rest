d91f6e905a280713424517334b0681282e97ead4
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-30 00:19:29 -0300

    feat: Register StockController blueprint in the main application

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@ from flask import Flask
 from asgi import start_server
 from settings import settings_from_server, type_server
 from utils.extensions import jwt
+
 from controllers.product_controller import product_controller
 from controllers.category_controller import category_controller
 from controllers.supplier_controller import supplier_controller
+from controllers.stock_controller import stock_controller
 
 # Flask
 # https://flask.palletsprojects.com/en/3.0.x/
@@ -16,6 +18,7 @@ app.config.from_object(settings_from_server[type_server])
 app.register_blueprint(product_controller)
 app.register_blueprint(category_controller)
 app.register_blueprint(supplier_controller)
+app.register_blueprint(stock_controller)
 
 # Extensions
 jwt.init_app(app)
