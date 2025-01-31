f525251582d89f21780ff823cc6cbc1330001ed5
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-01-31 18:54:08 -0300

    feat: Add new CategoryStatusError exception and improve existing category exceptions

--- src/exceptions/categories_exceptions.py
+++ src/exceptions/categories_exceptions.py
@@ -3,12 +3,21 @@ class CategoryException(Exception):
 
 
 class CategoryNotFound(CategoryException):
+
     def __init__(self, message = "Category does not found."):
         self.message = message
         super().__init__(self.message)
 
 
 class CategoryAlreadyExists(CategoryException):
+
     def __init__(self, message = "Category already exists."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class CategoryStatusError(CategoryException):
+    
+    def __init__(self, message = "Category already has status."):
         self.message = message
         super().__init__(self.message)
