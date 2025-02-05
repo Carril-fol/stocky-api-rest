73725f9e6782477224c71cd57765a104f5bce2fd
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-02-05 19:55:58 -0300

    refactor: Add StockHasAlreadyStatus exception for improved error handling

--- src/exceptions/stock_exceptions.py
+++ src/exceptions/stock_exceptions.py
@@ -6,3 +6,10 @@ class StockNotFound(StockException):
     def __init__(self, message="Stock not found."):
         self.message = message
         super().__init__(self.message)
+
+
+class StockHasAlreadyStatus(StockException):
+
+    def __init__(self, message="The entered stock already has the status"):
+        self.message = message
+        super().__init__(self.message)
