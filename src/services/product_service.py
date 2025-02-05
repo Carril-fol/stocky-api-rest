c42908ff7d68c298c63e5e6b600770bf9b94e141
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-02-05 19:55:39 -0300

    refactor: Update ProductService to use ProductModel consistently and improve status validation

--- src/services/product_service.py
+++ src/services/product_service.py
@@ -1,5 +1,6 @@
 from .service import BaseService
 from .stock_service import StockService
+
 from models.product_model import ProductModel
 from entities.product_entity import ProductEntity
 from repositories.product_repository import ProductRepository
@@ -21,7 +22,7 @@ class ProductService(BaseService):
             raise ProductNotFound()
         return product
     
-    def _validate_status_change(self, product, data: dict):
+    def _validate_status_change(self, product: ProductEntity, data: dict):
         status = data.get("status") 
         if status is not None and status == product.status:
             raise ProductHasAlreadyStatus()
@@ -32,12 +33,11 @@ class ProductService(BaseService):
         return self._validate_entity_and_serialize(product, self._product_model)
     
     def get_products(self):
-        products = self._product_repository.get_products()
-        for product in products:
+        for product in self._product_repository.get_products():
             yield self._validate_entity_and_serialize(product, self._product_model)
 
     def create_product_with_stock(self, data: dict):
-        product_data_validated = self._prepare_to_entity(data, ProductModel)
+        product_data_validated = self._prepare_to_entity(data, self._product_model)
         product_entity = ProductEntity(**product_data_validated.model_dump())
         product_created = self._product_repository.create_product(product_entity)
         product_created_model_dump = self._validate_entity_and_serialize(product_created, self._product_model)
@@ -46,17 +46,16 @@ class ProductService(BaseService):
     def update_product(self, id: int, data: dict):
         product = self._product_exist(id)
         self._validate_status_change(product, data)
-        product_to_update = self._prepare_to_entity(data, ProductModel, product)
+        product_to_update = self._prepare_to_entity(data, self._product_model, product)
         return self._product_repository.update_product(product_to_update)
 
     def delete_product(self, id: int, data: dict):
         product = self._product_exist(id)
-        self._validate_status_change(product, data["status"])
+        self._validate_status_change(product, data)
         self._stock_service.delete_stock_by_product_id(id)
-        product_to_delete = self._prepare_to_entity(data, ProductModel, product)
+        product_to_delete = self._prepare_to_entity(data, self._product_model, product)
         return self._product_repository.update_product(product_to_delete)
 
     def get_product_by_category_id(self, id: int):
-        products = self._product_repository.get_products_by_category_id(id)
-        for product in products:
+        for product in self._product_repository.get_products_by_category_id(id):
             yield self._validate_entity_and_serialize(product, self._product_model)
