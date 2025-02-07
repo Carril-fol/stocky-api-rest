ca33ac823c930885507693ebd445d35c53987c43
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-02-07 19:38:02 -0300

    refactor: Enhance API documentation for product endpoints with examples and response formats

--- src/controllers/product_controller.py
+++ src/controllers/product_controller.py
@@ -6,6 +6,25 @@ product_service = ProductService()
 
 @product_controller.route('/create', methods=['POST'])
 def create_product():
+    """
+    Example:
+
+    POST: /products/api/v1/create
+    ```
+    Application data:
+    {
+        'name': 'Name from the product',
+        'description': 'Description from the product',
+        'category_id': 'Id from the category to relacionate with the product',
+        'quantity': 'A number to set the quantity of the product in stock table'
+    }
+
+    Successful response (Code 201 - CREATED):
+    {
+        'msg': 'Product created successfully'
+    }
+    ```
+    """
     data = request.get_json()
     try:
         product_service.create_product_with_stock(data)
@@ -15,14 +34,50 @@ def create_product():
     
 @product_controller.route('/get/<int:id>', methods=['GET'])
 def detail_product(id: int):
+    """
+    Example:
+
+    GET: /products/api/v1/get/<int:id>
+    ```
+    Successful response (Code 200 - OK):
+    {
+        'product': {
+            'id': 'Id from the product',
+            'name': 'Name from the product',
+            'description': 'Description from the product',
+            'status': 'Status from the product',
+            'category_id': 'Id from the category to relacionate with the product',
+        }
+    }
+    ```
+    """
     try:
         product = product_service.get_product_by_id(id)
-        return make_response({'msg': product}, 200)
+        return make_response({'product': product}, 200)
     except Exception as error:
         return make_response({'error': str(error)}, 400)
     
 @product_controller.route('/update/<int:id>', methods=['PATCH', 'PUT'])
 def update_product(id: int):
+    """
+    Example:
+
+    PUT or PATCH: /products/api/v1/update/<int:id>
+    ```
+    Application data:
+    {
+        'name': 'New name from the product',
+        'description': 'New description from the product',
+        'status': 'New status from the product',
+        'category_id': 'New id from the category to relacionate with the product',
+    }
+
+    Successful response (Code 200 - OK):
+    {
+        'msg': 'Product updated successfully'
+    }
+    ```
+    """
     data = request.get_json()
     try:
         product_service.update_product(id, data)
@@ -32,6 +87,17 @@ def update_product(id: int):
     
 @product_controller.route('/delete/<int:id>', methods=['DELETE'])
 def delete_product(id: int):
+    """
+    Example:
+
+    DELETE: /products/api/v1/delete/<int:id>
+    ```
+    Successful response (Code 200 - OK):
+    {
+        'msg': 'Product deleted successfully'
+    }
+    ```
+    """
     data = {'status': 'inactive'}
     try:
         product_service.delete_product(id, data)
@@ -39,10 +105,30 @@ def delete_product(id: int):
     except Exception as error:
         return make_response({'error': str(error)}, 400)
     
-@product_controller.route('/', methods=['GET'])
+@product_controller.route('/get/all', methods=['GET'])
 def detail_products():
+    """
+    Example:
+
+    GET: /products/api/v1/get/all
+    ```
+    Successful response (Code 200 - OK):
+    {
+        'products': [
+            {
+                'id': 'Id from the product',
+                'name': 'Name from the product',
+                'description': 'Description from the product',
+                'status': 'Status from the product',
+                'category_id': 'Id from the category to relacionate with the product'
+            },
+            ...
+        ]
+    }
+    ```
+    """
     try:
         products = list(product_service.get_products())
-        return make_response({'msg': products}, 200)
+        return make_response({'products': products}, 200)
     except Exception as error:
         return make_response({'error': str(error)}, 400)
