efce19da9f6896ca0dca1ec307b73878d82d8f77
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-02-11 20:47:33 -0300

    refactor: Add new stock retrieval methods and improve stock querying logic

--- src/repositories/stock_repository.py
+++ src/repositories/stock_repository.py
@@ -1,14 +1,9 @@
 from repositories.repository import Repository
 from entities.stock_entity import StockEntity
+from entities.product_entity import ProductEntity
 
 class StockRepository(Repository):
-    
-    def get_stock_by_id(self, id: int):
-        return self.get_register_entity(StockEntity, id)
-
-    def get_stock(self):
-        return self.get_registers_entity(StockEntity)
-        
+            
     def create_stock(self, stock: StockEntity):
         return self.create_register_entity(stock)    
 
@@ -20,4 +15,19 @@ class StockRepository(Repository):
 
     def get_stock_by_product_id(self, product_id: int):
         with self.get_session() as session:
-            return session.query(StockEntity).filter(StockEntity.product_id == product_id).first()
+            return session.query(StockEntity).filter(StockEntity.product_id == product_id)
+
+    def get_stock_low_quantity(self):
+        with self.get_session() as session:
+            return session.query(StockEntity).filter(StockEntity.quantity < 10).all()
+        
+    def get_stock_detailed(self, page, per_page):
+        with self.get_session() as session:
+            query = session.query(StockEntity, ProductEntity).join(ProductEntity, StockEntity.product_id == ProductEntity.id)
+            return query.limit(per_page).offset((page - 1) * per_page).all()
+
+    def get_stock_detailed_by_id(self, id: int):
+        with self.get_session() as session:
+            return session.query(StockEntity, ProductEntity).join(
+                ProductEntity, StockEntity.product_id == ProductEntity.id
+            ).filter(StockEntity.id == id).first()
