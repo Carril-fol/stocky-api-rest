5c20124890e4ae21f2d76d97065076a3372fb28a
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2025-02-25 18:38:43 -0300

    feat: Added new controller

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ from controllers.product_controller import product_controller
 from controllers.category_controller import category_controller
 from controllers.supplier_controller import supplier_controller
 from controllers.stock_controller import stock_controller
+from controllers.core_controller import core_controller
 
 # Flask
 # https://flask.palletsprojects.com/en/3.0.x/
@@ -18,6 +19,7 @@ app.register_blueprint(product_controller)
 app.register_blueprint(category_controller)
 app.register_blueprint(supplier_controller)
 app.register_blueprint(stock_controller)
+app.register_blueprint(core_controller)
 
 if __name__ == "__main__":
     start_server(app)
