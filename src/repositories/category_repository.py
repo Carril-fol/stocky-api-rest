969ff8a1e9562701e65c42e58f8d25afa4f4154e
Folco Carril <folco.carril@gmail.com>
2025-10-15 16:36:44 -0300

    chore(CategoryRepository): Added what type return functions in repository

--- src/repositories/category_repository.py
+++ src/repositories/category_repository.py
@@ -3,22 +3,22 @@ from entities.category_entity import CategoryEntity
 
 class CategoryRepository(Repository):
 
-    def get_category_by_id(self, id: int):
+    def get_category_by_id(self, id: int) -> CategoryEntity:
         return self.get_register_entity(CategoryEntity, id)
         
-    def get_categories(self):
+    def get_categories(self) -> list[CategoryEntity]:
         return self.get_registers_entity(CategoryEntity)
 
-    def create_category(self, category: CategoryEntity):
+    def create_category(self, category: CategoryEntity) -> CategoryEntity:
         return self.create_register_entity(category)
     
-    def update_category(self, category: CategoryEntity):
+    def update_category(self, category: CategoryEntity) -> CategoryEntity:
         return self.update_register_entity(category)
 
-    def delete_category(self, category: CategoryEntity):
+    def delete_category(self, category: CategoryEntity) -> CategoryEntity:
         return self.delete_logic_register_entity(category)
     
-    def get_category_by_name(self, name: str):
+    def get_category_by_name(self, name: str) -> CategoryEntity | None:
         with self.get_session() as session:
             return session.query(CategoryEntity).filter(CategoryEntity.name == name).first()
         
