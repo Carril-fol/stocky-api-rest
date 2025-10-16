1063a27af55c1ae675b169c4afe23aea949781ff
Folco Carril <folco.carril@gmail.com>
2025-10-15 23:43:59 -0300

    feat(SupplierModel): Model created to update and create

--- src/models/supplier_model.py
+++ src/models/supplier_model.py
@@ -2,22 +2,33 @@ from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 
 class SupplierModel(BaseModel):
-    id: Optional[int] = Field(default=None, description='ID of the supplier')
-    name: Optional[str] = Field(default=None, min_length=3, max_length=50, description='Name of the supplier')
-    status: Optional[str] = Field(default='active', description="Status of the supplier, defaults to 'active'")
-
-    @field_validator('name')
-    def name_must_contain_alphabet_characters(cls, value):
-        if not any((str(character)).isalpha() for character in value):
-            raise ValueError('name must contain alphabet characters')
-        return value
-
-    @field_validator('status')
-    def status_must_be_active_or_inactive(cls, value):
-        if value not in ['active', 'inactive']:
-            raise ValueError('status must be active or inactive')
-        return value 
+    id: int = Field(default=None, description='ID of the supplier')
+    name: str = Field(default=None, min_length=3, max_length=50, description='Name of the supplier')
+    status: str = Field(default='ACTIVE', description="Status of the supplier, defaults to 'active'")
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {int: str}
+
+
+class CreateUpdateSupplierModel(BaseModel):
+    name: str = Field(default=None, min_length=3, max_length=120, description='Name of the supplier')
+    status: Optional[str] = Field(default='ACTIVE', description="Status of the supplier, defaults to 'active'")
+
+    @field_validator('*', mode='before')
+    def convert_to_uppercase(cls, value):
+        if isinstance(value, str):
+            return value.upper()
+        return value
+
+    @field_validator('name')
+    def name_must_contain_alphabet_characters(cls, value: str):
+        if not any(character.isalpha() for character in value):
+            raise ValueError('name must contain alphabet characters')
+        return value
+
+    @field_validator('status')
+    def status_must_be_active_or_inactive(cls, value: str):
+        if value not in ['ACTIVE', 'INACTIVE']:
+            raise ValueError("status must be active or inactive")
+        return value
