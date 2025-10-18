8bdbbd678a74c4e2831546bee725d02571e8401f
Folco Carril <folco.carril@gmail.com>
2025-10-18 02:12:30 -0300

    feat(asgi): Init values in tables in database

--- src/asgi.py
+++ src/asgi.py
@@ -3,11 +3,29 @@ from asgiref.wsgi import WsgiToAsgi
 from dotenv import load_dotenv
 
 from database.db import Database
+from repositories.supplier_repository import SupplierRepository
+from repositories.category_repository import CategoryRepository
+from repositories.product_repository import ProductRepository
+
+from services.supplier_service import SupplierService
+from services.category_service import CategoryService
 load_dotenv()
+
+def values_init_in_database():
+    supplier_repository = SupplierRepository()
+    supplier_service = SupplierService(supplier_repository)
+
+    product_repository = ProductRepository()
+    category_repository = CategoryRepository()
+    category_service = CategoryService(category_repository, product_repository)
+
+    supplier_service.create_default_supplier()
+    category_service.create_default_category()
 
 def start_server(app):
     db = Database()
     db.initialize()
+    values_init_in_database()
 
     host = app.config.get("SERVER_HOST")
     port = app.config.get("SERVER_PORT")
