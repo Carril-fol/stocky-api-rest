5201267e9da3ff5f0cee0e04d422bab12767ad80
Folco Carril <folco.carril@gmail.com>
2026-06-12 17:42:02 -0300

    chore(PermissionEntity): Added atribute in field name from entity permission to be unique

--- src/modules/permissions/permissions_entity.py
+++ src/modules/permissions/permissions_entity.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class PermissionsEntity(Base):
     __tablename__ = "permissions"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
 
     def to_dict(self):
         return {
