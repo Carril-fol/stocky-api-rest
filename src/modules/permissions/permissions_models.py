8f5cf3660afc7abeef2588b9d3cb7eda70509a47
Folco Carril <folco.carril@gmail.com>
2026-06-12 17:49:19 -0300

    chore(PermissionModel): added function to convert name from lower case to upper case

--- src/modules/permissions/permissions_models.py
+++ src/modules/permissions/permissions_models.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
 
 class CRUDPermissionBase(BaseModel):
-    name: str
+    name: str = Field(..., min_length=3, max_length=100)
+
+    @field_validator("*", mode="before")
+    def convert_to_uppercase(cls, value):
+        if isinstance(value, str):
+            return value.upper()
+        return value
 
 
 class PermissionModel(CRUDPermissionBase):
